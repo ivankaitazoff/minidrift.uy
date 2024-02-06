f6a644fc76fa71d4a3830367e640c63af5e0e486
ivankaitazoff <ivankaitazoff@gmail.com>
2024-02-06 09:54:32 -0300

    Fixed timer with old JS file

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
+// Set the date we're counting down to
+//Moved this to the HTML
 //var countDownDate = new Date().getTime() + 15 * 60 * 1000;
 
-function setInterval(countdownJSVariableName,timerId){
-    // Update the count down every 1 second
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-    // Get today's date and time
-    var now = new Date().getTime();
-  
-    // Find the distance between now and the count down date
-    var distance = countdownJSVariableName - now;
-  
-    // Time calculations for hours, minutes and seconds
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-    // Display the result in the element with id="demo"
-    document.getElementById(timerId).innerHTML =  hours + ":"
-    + minutes + ":" + seconds;
-  
-    // If the count down is finished, write some text
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById(timerId).innerHTML = "Tiempo Terminado";
-    }
-}
+  // Get today's date and time
+  var now = new Date().getTime();
 
-var x = setInterval("countDownDateP1","TimerPlayer1");
+  // Find the distance between now and the count down date
+  var distance = countDownDateP1 - now;
+
+  // Time calculations for hours, minutes and seconds
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("TimerPlayer1").innerHTML =  hours + ":"
+  + minutes + ":" + seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("TimerPlayer1").innerHTML = "Tiempo Terminado";
+  }
+}, 1000);
 
