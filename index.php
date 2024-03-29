ba8c0f0419eab51aa00f7f94bd1fe295497bbb4c
ivankaitazoff <ivankaitazoff@gmail.com>
2024-03-29 11:30:56 -0300

    background and text color set, need to align text and find font

--- index.php
+++ index.php
@@ -7,39 +7,30 @@
   <title>Document</title>
   <!-- Display the countdown timer in an element -->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
 <body>
     
 
   <!--img src="./images/miniDriftLogoAzulIcon.png" alt="Mini Drift Logo"-->
-  <!div class="container">
+  <div class="text_names_time">
   <div class="row justify-content-md-left">
-    <img src=images/GTRChico.jpg class="col col-lg-3">
-    <h1 class="col-lg-3 align-self-center" id="p1Name"> P1 Name </h1>
-    <h4 class="col col-lg-3 align-self-center"> Tiempo restante: </h4>
-    
-    <h1 id="TimerPlayer1" class="col col-lg-3 align-self-center">TimerPlayer1</h1>
+    <h1 class="text-white" id="p1Name"> P1 Name </h1>    
+    <h1 id="TimerPlayer1" class="text-white">TimerPlayer1</h1>
   </div>
 
   <div class="row justify-content-md-left">
-    <img src=images/SubaruSTIChico.jpg class="col col-lg-3">
-    <h1 class="col-lg-3 align-self-center" id="p2Name" class="text-center"> P2 Name </h1>
-    <h4 class="col col-lg-3 align-self-center"> Tiempo restante: </h4>
-    <h1 id="TimerPlayer2" class="col col-lg-3 align-self-center">TimerPlayer2</h1>
+    <h1 class="text-white" id="p2Name" class="text-center"> P2 Name </h1>
+    <h1 id="TimerPlayer2" class="text-white">TimerPlayer2</h1>
   </div>
 
   <div class="row justify-content-md-left">
-    <img src=images/ToyotaSupraChico.jpg class="col col-lg-3">
-    <h1 class="col-lg-3 align-self-center" id="p3Name"> P3 Name </h1>
-    <h4 class="col col-lg-3 align-self-center"> Tiempo restante: </h4>
-    <h1 id="TimerPlayer3" class="col col-lg-3 align-self-center">TimerPlayer3</h1>
+    <h1 class="text-white" id="p3Name"> P3 Name </h1>
+    <h1 id="TimerPlayer3" class="text-white">TimerPlayer3</h1>
   </div>
 
   <div class="row justify-content-md-left">
-    <img src=images/GTRChico.jpg class="col col-lg-3">
-    <h1 class="col-lg-3 align-self-center" id="p4Name"> P4 Name </h1>
-    <h4 class="col col-lg-3 align-self-center"> Tiempo restante: </h4>
-    <h1 id="TimerPlayer4" class="col col-lg-3 align-self-center">TimerPlayer4</h1>
+    <h1 class="text-white" id="p4Name"> P4 Name </h1>
+    <h1 id="TimerPlayer4" class="text-white">TimerPlayer4</h1>
   </div>
 
   </div>
@@ -48,7 +39,7 @@
   
   
   <script src="script.js"></script>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
