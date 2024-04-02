301aee5d4eed77279b381e9c7d1f6cfa1bc37574
ivankaitazoff <ivankaitazoff@gmail.com>
2024-04-02 08:57:55 -0300

    Added file that contains the time selected(15 or 30),and small fixes

--- action_page.php
+++ action_page.php
@@ -20,7 +20,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the parameters exist in the request
     if (isset($_POST['Jugador1Checkbox']) && $_POST['Jugador1Checkbox'] === 'true') {
         $Player1NameFile = './currentPlayers/Player1Name.txt';
-        $Player1TimeFile = './currentPlayers/Player1EndTime.txt';
+        $Player1EndTimeFile = './currentPlayers/Player1EndTime.txt';
+        $Player1TimeFile = './currentPlayers/Player1Time.txt';
         // Retrieve the values of the parameters
         $nombreP1 = $_POST['nombreP1'];
         $tiempoP1 = $_POST['tiempoP1'];
@@ -29,9 +30,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         file_put_contents($Player1NameFile, $nombreP1);
         
         //Save time to a file
-        $endPlayer1Time = $t + $tiempoP1 * 60 * 1000;
-        file_put_contents($Player1TimeFile, $endPlayer1Time);
-
+        $endPlayer1Time = $t + $tiempoP1 * 60 * 1000 + 5000;
+        file_put_contents($Player1EndTimeFile, $endPlayer1Time);
+        file_put_contents($Player1TimeFile, $tiempoP1);
         // Now you can use these variables as needed
         echo "Jugador 1 Actualizado<br>";
         echo "Nombre: " . $nombreP1 . "<br>";
@@ -44,18 +45,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the parameters exist in the request
     if (isset($_POST['Jugador2Checkbox']) && $_POST['Jugador2Checkbox'] === 'true') {
         $Player2NameFile = './currentPlayers/Player2Name.txt';
-        $Player2TimeFile = './currentPlayers/Player2EndTime.txt';
+        $Player2EndTimeFile = './currentPlayers/Player2EndTime.txt';
+        $Player2TimeFile = './currentPlayers/Player2Time.txt';
         // Retrieve the values of the parameters
         $nombreP2 = $_POST['nombreP2'];
         $tiempoP2 = $_POST['tiempoP2'];
         
         // Save the value to the file
         file_put_contents($Player2NameFile, $nombreP2);
-        
         //Save time to a file
-        $endPlayer2Time = $t + ($tiempoP2 * 60 * 1000);
-        file_put_contents($Player2TimeFile, $endPlayer2Time);
-
+        $endPlayer2Time = $t + ($tiempoP2 * 60 * 1000 + 5000);
+        file_put_contents($Player2EndTimeFile, $endPlayer2Time);
+        file_put_contents($Player2TimeFile, $tiempoP2);
         // Now you can use these variables as needed
         echo "Jugador 2 Actualizado<br>";
         echo "Nombre: " . $nombreP2 . "<br>";
@@ -68,7 +69,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the parameters exist in the request
     if (isset($_POST['Jugador3Checkbox']) && $_POST['Jugador3Checkbox'] === 'true') {
         $Player3NameFile = './currentPlayers/Player3Name.txt';
-        $Player3TimeFile = './currentPlayers/Player3EndTime.txt';
+        $Player3EndTimeFile = './currentPlayers/Player3EndTime.txt';
+        $Player3TimeFile = './currentPlayers/Player3Time.txt';
         // Retrieve the values of the parameters
         $nombreP3 = $_POST['nombreP3'];
         $tiempoP3 = $_POST['tiempoP3'];
@@ -77,8 +79,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         file_put_contents($Player3NameFile, $nombreP3);
         
         //Save time to a file
-        $endPlayer3Time = $t + ($tiempoP3 * 60 * 1000);
-        file_put_contents($Player3TimeFile, $endPlayer3Time);
+        $endPlayer3Time = $t + ($tiempoP3 * 60 * 1000 + 5000);
+        file_put_contents($Player3EndTimeFile, $endPlayer3Time);
+        file_put_contents($Player3TimeFile, $tiempoP3);
 
         // Now you can use these variables as needed
         echo "Jugador 3 Actualizado<br>";
@@ -92,7 +95,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the parameters exist in the request
     if (isset($_POST['Jugador4Checkbox']) && $_POST['Jugador4Checkbox'] === 'true') {
         $Player4NameFile = './currentPlayers/Player4Name.txt';
-        $Player4TimeFile = './currentPlayers/Player4EndTime.txt';
+        $Player4EndTimeFile = './currentPlayers/Player4EndTime.txt';
+        $Player4TimeFile = './currentPlayers/Player4Time.txt';
         // Retrieve the values of the parameters
         $nombreP4 = $_POST['nombreP4'];
         $tiempoP4 = $_POST['tiempoP4'];
@@ -101,8 +105,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         file_put_contents($Player4NameFile, $nombreP4);
         
         //Save time to a file
-        $endPlayer4Time = $t + ($tiempoP4 * 60 * 1000);
-        file_put_contents($Player4TimeFile, $endPlayer4Time);
+        $endPlayer4Time = $t + ($tiempoP4 * 60 * 1000 + 5000);
+        file_put_contents($Player4EndTimeFile, $endPlayer4Time);
+        file_put_contents($Player4TimeFile, $tiempoP4);
 
         // Now you can use these variables as needed
         echo "Jugador 4 Actualizado<br>";
--- getUpdate.php
+++ getUpdate.php
@@ -12,27 +12,35 @@ $player2Name = file_get_contents('./currentPlayers/Player2Name.txt', true);
 $player3Name = file_get_contents('./currentPlayers/Player3Name.txt', true);
 $player4Name = file_get_contents('./currentPlayers/Player4Name.txt', true);
 
-//$endPlayer1Time = $t + 4 * 60 * 1000;
+
 $endPlayer1Time = file_get_contents('./currentPlayers/Player1EndTime.txt', true);
-//$endPlayer2Time = $t + 2 * 60 * 1000;
+$player1Time = file_get_contents('./currentPlayers/Player1Time.txt', true);
+
 $endPlayer2Time = file_get_contents('./currentPlayers/Player2EndTime.txt', true);
-//$endPlayer3Time = $t + 3 * 60 * 1000;
+$player2Time = file_get_contents('./currentPlayers/Player2Time.txt', true);
+
 $endPlayer3Time = file_get_contents('./currentPlayers/Player3EndTime.txt', true);
-//$endPlayer4Time = $t + 4 * 60 * 1000;
+$player3Time = file_get_contents('./currentPlayers/Player3Time.txt', true);
+
 $endPlayer4Time = file_get_contents('./currentPlayers/Player4EndTime.txt', true);
+$player4Time = file_get_contents('./currentPlayers/Player4Time.txt', true);
 
 //Print Player 1
 echo "<p id='player1Name'>$player1Name</p>";
 echo "<p id='endPlayer1Time'>$endPlayer1Time</p>";
+echo "<p id='player1Time'>$player1Time</p>";
 //Print Player 2
 echo "<p id='player2Name'>$player2Name</p>";
 echo "<p id='endPlayer2Time'>$endPlayer2Time</p>";
+echo "<p id='player2Time'>$player2Time</p>";
 //Print Player 3
 echo "<p id='player3Name'>$player3Name</p>";
 echo "<p id='endPlayer3Time'>$endPlayer3Time</p>";
+echo "<p id='player3Time'>$player3Time</p>";
 //Print Player 4
 echo "<p id='player4Name'>$player4Name</p>";
 echo "<p id='endPlayer4Time'>$endPlayer4Time</p>";
+echo "<p id='player4Time'>$player4Time</p>";
 ?>
 </body>
 </html>
